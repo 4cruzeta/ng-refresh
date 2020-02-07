ac9bf64effc663e71441e57ed05c0b9433d5c1b6
4cruzeta <4cruzetam@gmail.com>
2020-02-07 09:42:43 -0300

    Services com Dependency Injection

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { PersonsService } from './persons.service';
 
 @Component({                                    // Diz ao Angular que isso é um Component. @ - decorator
   selector: 'app-persons',                      // convenção usar prefixo app, -, nome do componente
@@ -6,5 +7,9 @@ import { Component, Input } from '@angular/core';
 
 })
 export class PersonsComponent {
-  @Input() personList: string[];
+personList: string[];
+
+constructor(prsService: PersonsService) {
+  this.personList = prsService.persons;
+}
 }
